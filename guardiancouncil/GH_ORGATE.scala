88180b6d39fdf29f583afa0666b288d6b160e26e
zhejiang <zhejiang.uk@gmail.com>
2022-09-24 12:48:12 +0100

    Fixed the generic issue for or gates.

--- guardiancouncil/GH_ORGATE.scala
+++ guardiancouncil/GH_ORGATE.scala
@@ -19,24 +19,6 @@ trait HasORGATEIO extends BaseModule {
 }
 
 class GH_ORGATE (val params: ORGATEParams) extends Module with HasORGATEIO {
-  val out_wire                                  = WireInit (0.U (params.width.W))
-
-  /* This method reports a combinational loop, which is expected.
-     We use the mannual method to avoid masking the above error
-  */ 
-
-  /* for (i <- 0 to params.number-1) {
-      out_wire                                 := out_wire | io.in(i)
-    }
-  */
-
-  io.out                                       := io.in(0) |
-                                                  io.in(1) |
-                                                  io.in(2) |
-                                                  io.in(3) |
-                                                  io.in(4) |
-                                                  io.in(5) |
-                                                  io.in(6) |
-                                                  io.in(7)
+  io.out                                       := io.in(0).reduce(_|_)
 }
 
