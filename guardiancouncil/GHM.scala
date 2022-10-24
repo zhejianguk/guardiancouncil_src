726024d9c36a494bae34d3867b2e258a65c3b074
zhejiang <zhejiang.uk@gmail.com>
2022-10-24 23:17:13 +0100

    Re-design the GHM to support clock routing.

--- guardiancouncil/GHM.scala
+++ guardiancouncil/GHM.scala
@@ -5,8 +5,9 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental.{BaseModule}
 import freechips.rocketchip.config.{Field, Parameters}
-import freechips.rocketchip.subsystem.{BaseSubsystem, HierarchicalLocation, HasTiles}
+import freechips.rocketchip.subsystem.{BaseSubsystem, HierarchicalLocation, HasTiles, TLBusWrapperLocation}
 import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.prci.{ClockSinkDomain}
 
 
 
@@ -49,97 +50,100 @@ trait HasGHMIO extends BaseModule {
 //==========================================================
 // Implementations
 //==========================================================
-class GHM (val params: GHMParams) extends Module with HasGHMIO
+class GHM (val params: GHMParams)(implicit p: Parameters) extends LazyModule
 {
-  val u_agg                                      = Module (new GHT_AGG(GHM_AGG_Params(params.number_of_little_cores, params.width_GH_packet)))
+  lazy val module = new LazyModuleImp(this) {
+    val io                                         = IO(new GHMIO(params))
+    val u_agg                                      = Module (new GHT_AGG(GHM_AGG_Params(params.number_of_little_cores, params.width_GH_packet)))
 
-  // Adding a register to avoid the critical path
-  val packet_in_reg                              = RegInit (0.U(params.width_GH_packet.W))
-  val packet_dest_reg                            = RegInit (0.U((params.number_of_little_cores+1).W))
-  val packet_out_wires                           = WireInit (VecInit(Seq.fill(params.number_of_little_cores)(0.U(params.width_GH_packet.W))))
-  packet_in_reg                                 := io.ghm_packet_in
-  packet_dest_reg                               := io.ghm_packet_dest
-  
-  var warning                                    = WireInit(0.U(1.W))
-  var complete                                   = WireInit(1.U(1.W))
-  var release                                    = WireInit(1.U(1.W))
-  var sch_na                                     = WireInit(0.U(params.number_of_little_cores.W))
+    // Adding a register to avoid the critical path
+    val packet_in_reg                              = RegInit (0.U(params.width_GH_packet.W))
+    val packet_dest_reg                            = RegInit (0.U((params.number_of_little_cores+1).W))
+    val packet_out_wires                           = WireInit (VecInit(Seq.fill(params.number_of_little_cores)(0.U(params.width_GH_packet.W))))
+    packet_in_reg                                 := io.ghm_packet_in
+    packet_dest_reg                               := io.ghm_packet_dest
+    
+    var warning                                    = WireInit(0.U(1.W))
+    var complete                                   = WireInit(1.U(1.W))
+    var release                                    = WireInit(1.U(1.W))
+    var sch_na                                     = WireInit(0.U(params.number_of_little_cores.W))
 
-  val sch_na_in_wires                            = WireInit(VecInit(Seq.fill(params.number_of_little_cores)(0.U(params.number_of_little_cores.W))))
-  val zeros_nbit                                 = WireInit(0.U((params.number_of_little_cores - 1).W))
-  val agg_core_id                                = io.agg_core_id - 1.U
-  val do_refresh                                 = WireInit(0.U(32.W))
+    val sch_na_in_wires                            = WireInit(VecInit(Seq.fill(params.number_of_little_cores)(0.U(params.number_of_little_cores.W))))
+    val zeros_nbit                                 = WireInit(0.U((params.number_of_little_cores - 1).W))
+    val agg_core_id                                = io.agg_core_id - 1.U
+    val do_refresh                                 = WireInit(0.U(32.W))
 
-  val debug_gcounter                             = RegInit(0.U(64.W))
+    val debug_gcounter                             = RegInit(0.U(64.W))
 
-  
-  
-  // Routing
-  for (i <- 0 to params.number_of_little_cores - 1) {
-    packet_out_wires(i)                         := MuxCase(0.U, 
-                                                    Array(((packet_dest_reg(i) === 1.U) && (agg_core_id =/= i.U)) -> packet_in_reg,
-                                                          ((packet_dest_reg(i) =/= 1.U) && (agg_core_id =/= i.U)) -> 0.U,
-                                                          (agg_core_id === i.U) -> u_agg.io.agg_packet_out
-                                                          )
-                                                          ) 
-    u_agg.io.agg_packet_in(i)                   := io.agg_packet_in(i)
-    io.agg_buffer_full(i)                       := u_agg.io.agg_buffer_full(i)
+    
+    
+    // Routing
+    for (i <- 0 to params.number_of_little_cores - 1) {
+      packet_out_wires(i)                         := MuxCase(0.U, 
+                                                      Array(((packet_dest_reg(i) === 1.U) && (agg_core_id =/= i.U)) -> packet_in_reg,
+                                                            ((packet_dest_reg(i) =/= 1.U) && (agg_core_id =/= i.U)) -> 0.U,
+                                                            (agg_core_id === i.U) -> u_agg.io.agg_packet_out
+                                                            )
+                                                            ) 
+      u_agg.io.agg_packet_in(i)                   := io.agg_packet_in(i)
+      io.agg_buffer_full(i)                       := u_agg.io.agg_buffer_full(i)
+    }
+    u_agg.io.agg_core_full                        := io.agg_core_status(agg_core_id)(1)
+    do_refresh                                    := io.sch_do_refresh(agg_core_id)
+
+    // 2-cyecle delays are added to ensure all filtering activities are completed.
+    val ghm_status_delay1_cycle                    = RegInit(0.U(2.W))
+    val ghm_status_delay2_cycle                    = RegInit(0.U(2.W))
+    val ghm_status_delay3_cycle                    = RegInit(0.U(2.W))
+    val ghm_status_delay4_cycle                    = RegInit(0.U(2.W))
+    ghm_status_delay1_cycle                       := io.ghm_status_in(1,0)
+    ghm_status_delay2_cycle                       := ghm_status_delay1_cycle
+    ghm_status_delay3_cycle                       := ghm_status_delay2_cycle
+    ghm_status_delay4_cycle                       := ghm_status_delay3_cycle
+
+
+    val collecting_checker_status                  = io.agg_core_status.reduce(_&_)
+    val if_checkers_empty                          = collecting_checker_status(0)
+    val if_filters_empty                           = io.ghm_status_in(31)
+    val if_ghm_empty                               = Mux(((packet_dest_reg === 0.U) && (io.ghm_packet_dest === 0.U)), 1.U, 0.U)
+
+    val if_no_inflight_packets                     = if_checkers_empty & if_filters_empty & if_ghm_empty
+    val zeros_62bit                                = WireInit(0.U(62.W))
+    for(i <- 0 to params.number_of_little_cores - 1) {
+      io.ghm_packet_outs(i)                       := packet_out_wires(i)
+      io.ghm_status_outs(i)                       := Mux((if_no_inflight_packets === 1.U), Cat(zeros_62bit, ghm_status_delay4_cycle), 1.U)
+      sch_na_in_wires(i)                          := Cat(zeros_nbit, io.sch_na_in(i))
+      io.sch_refresh_out(i)                       := do_refresh(i)
+    }
+
+
+    for(i <- 0 to params.number_of_little_cores - 1) {
+      warning                                     = warning | io.ghe_event_in(i)(0)
+      complete                                    = complete & io.ghe_event_in(i)(1)
+      release                                     = release & io.ghe_event_in(i)(2)
+      sch_na                                      = sch_na | (sch_na_in_wires(i) << i)
+    }
+
+    when (io.ghm_packet_dest =/= 0.U) {
+      debug_gcounter                             := debug_gcounter + 1.U
+    }
+
+    io.bigcore_hang                              := warning
+    io.bigcore_comp                              := Cat(release, complete)
+    io.sch_na_out                                := sch_na
+
+    io.debug_gcounter                            := debug_gcounter
   }
-  u_agg.io.agg_core_full                        := io.agg_core_status(agg_core_id)(1)
-  do_refresh                                    := io.sch_do_refresh(agg_core_id)
-
-  // 2-cyecle delays are added to ensure all filtering activities are completed.
-  val ghm_status_delay1_cycle                    = RegInit(0.U(2.W))
-  val ghm_status_delay2_cycle                    = RegInit(0.U(2.W))
-  val ghm_status_delay3_cycle                    = RegInit(0.U(2.W))
-  val ghm_status_delay4_cycle                    = RegInit(0.U(2.W))
-  ghm_status_delay1_cycle                       := io.ghm_status_in(1,0)
-  ghm_status_delay2_cycle                       := ghm_status_delay1_cycle
-  ghm_status_delay3_cycle                       := ghm_status_delay2_cycle
-  ghm_status_delay4_cycle                       := ghm_status_delay3_cycle
-
-
-  val collecting_checker_status                  = io.agg_core_status.reduce(_&_)
-  val if_checkers_empty                          = collecting_checker_status(0)
-  val if_filters_empty                           = io.ghm_status_in(31)
-  val if_ghm_empty                               = Mux(((packet_dest_reg === 0.U) && (io.ghm_packet_dest === 0.U)), 1.U, 0.U)
-
-  val if_no_inflight_packets                     = if_checkers_empty & if_filters_empty & if_ghm_empty
-  val zeros_62bit                                = WireInit(0.U(62.W))
-  for(i <- 0 to params.number_of_little_cores - 1) {
-    io.ghm_packet_outs(i)                       := packet_out_wires(i)
-    io.ghm_status_outs(i)                       := Mux((if_no_inflight_packets === 1.U), Cat(zeros_62bit, ghm_status_delay4_cycle), 1.U)
-    sch_na_in_wires(i)                          := Cat(zeros_nbit, io.sch_na_in(i))
-    io.sch_refresh_out(i)                       := do_refresh(i)
-  }
-
-
-  for(i <- 0 to params.number_of_little_cores - 1) {
-    warning                                     = warning | io.ghe_event_in(i)(0)
-    complete                                    = complete & io.ghe_event_in(i)(1)
-    release                                     = release & io.ghe_event_in(i)(2)
-    sch_na                                      = sch_na | (sch_na_in_wires(i) << i)
-  }
-
-  when (io.ghm_packet_dest =/= 0.U) {
-    debug_gcounter                             := debug_gcounter + 1.U
-  }
-
-  io.bigcore_hang                              := warning
-  io.bigcore_comp                              := Cat(release, complete)
-  io.sch_na_out                                := sch_na
-
-  io.debug_gcounter                            := debug_gcounter
 }
 
 case class GHMCoreLocated(loc: HierarchicalLocation) extends Field[Option[GHMParams]](None)
 
 object GHMCore {
 
-  def attach(params: GHMParams, subsystem: BaseSubsystem with HasTiles)
+  def attach(params: GHMParams, subsystem: BaseSubsystem with HasTiles, where: TLBusWrapperLocation)
             (implicit p: Parameters) {
     val number_of_ghes                             = subsystem.tile_ghe_packet_in_EPNodes.size
-
+  
     // Creating nodes for connections.
     val bigcore_hang_SRNode                        = BundleBridgeSource[UInt](Some(() => UInt(1.W)))
     val bigcore_comp_SRNode                        = BundleBridgeSource[UInt](Some(() => UInt(2.W)))
@@ -215,13 +219,16 @@ object GHMCore {
     val debug_gcounter_SRNode                      = BundleBridgeSource[UInt](Some(() => UInt(64.W)))
     subsystem.tile_debug_gcounter_EPNode          := debug_gcounter_SRNode
     
+    val bus = subsystem.locateTLBusWrapper(where)
+    val ghm = LazyModule (new GHM (GHMParams (params.number_of_little_cores, params.width_GH_packet, params.xlen, params.id_agg)))
 
+    
     InModuleBody {
-      val ghm = Module (new GHM (GHMParams (params.number_of_little_cores, params.width_GH_packet, params.xlen, params.id_agg)))
-      ghm.io.ghm_packet_in                        := ghm_ght_packet_in_SKNode.bundle
-      ghm.io.ghm_packet_dest                      := ghm_ght_packet_dest_SKNode.bundle
-      ghm.io.ghm_status_in                        := ghm_ght_status_in_SKNode.bundle
-      ghm.io.agg_core_id                          := ghm_agg_core_id_SKNode.bundle 
+      ghm.module.clock                            := bus.module.clock
+      ghm.module.io.ghm_packet_in                 := ghm_ght_packet_in_SKNode.bundle
+      ghm.module.io.ghm_packet_dest               := ghm_ght_packet_dest_SKNode.bundle
+      ghm.module.io.ghm_status_in                 := ghm_ght_status_in_SKNode.bundle
+      ghm.module.io.agg_core_id                   := ghm_agg_core_id_SKNode.bundle 
 
       for (i <- 0 to number_of_ghes-1) {
         if (i == 0) { // The big core
@@ -230,22 +237,23 @@ object GHMCore {
           ghm_ghe_status_out_SRNodes(i).bundle    := 0.U
           agg_buffer_full_out_SRNodes(i).bundle   := 0.U
         } else {// -1 big core
-          ghm_ghe_packet_out_SRNodes(i).bundle    := ghm.io.ghm_packet_outs(i-1)
-          ghm.io.agg_core_status(i-1)             := agg_core_status_in_SKNodes(i).bundle
-          ghm.io.sch_do_refresh(i-1)              := ghm_ght_sch_dorefresh_in_SKNodes(i).bundle
-          ghm_ghe_status_out_SRNodes(i).bundle    := ghm.io.ghm_status_outs(i-1)
-          ghm.io.ghe_event_in(i-1)                := ghm_ghe_event_in_SKNodes(i).bundle
-          ghm.io.agg_packet_in(i-1)               := agg_packet_in_SKNodes(i).bundle
-          agg_buffer_full_out_SRNodes(i).bundle   := ghm.io.agg_buffer_full(i-1)
-          ghm.io.sch_na_in(i-1)                   := ghm_ght_sch_na_in_SKNodes(i).bundle
-          ghm_ghe_sch_refresh_out_SRNodes(i).bundle:= ghm.io.sch_refresh_out(i-1)
+          ghm_ghe_packet_out_SRNodes(i).bundle    := ghm.module.io.ghm_packet_outs(i-1)
+          ghm.module.io.agg_core_status(i-1)      := agg_core_status_in_SKNodes(i).bundle
+          ghm.module.io.sch_do_refresh(i-1)       := ghm_ght_sch_dorefresh_in_SKNodes(i).bundle
+          ghm_ghe_status_out_SRNodes(i).bundle    := ghm.module.io.ghm_status_outs(i-1)
+          ghm.module.io.ghe_event_in(i-1)         := ghm_ghe_event_in_SKNodes(i).bundle
+          ghm.module.io.agg_packet_in(i-1)        := agg_packet_in_SKNodes(i).bundle
+          agg_buffer_full_out_SRNodes(i).bundle   := ghm.module.io.agg_buffer_full(i-1)
+          ghm.module.io.sch_na_in(i-1)            := ghm_ght_sch_na_in_SKNodes(i).bundle
+          ghm_ghe_sch_refresh_out_SRNodes(i).bundle:= ghm.module.io.sch_refresh_out(i-1)
         }
       }
 
-      bigcore_hang_SRNode.bundle                  := ghm.io.bigcore_hang
-      bigcore_comp_SRNode.bundle                  := ghm.io.bigcore_comp
-      sch_na_SRNode.bundle                        := ghm.io.sch_na_out
-      debug_gcounter_SRNode.bundle                := ghm.io.debug_gcounter
+      bigcore_hang_SRNode.bundle                  := ghm.module.io.bigcore_hang
+      bigcore_comp_SRNode.bundle                  := ghm.module.io.bigcore_comp
+      sch_na_SRNode.bundle                        := ghm.module.io.sch_na_out
+      debug_gcounter_SRNode.bundle                := ghm.module.io.debug_gcounter
     }
+    ghm
   }
 }
