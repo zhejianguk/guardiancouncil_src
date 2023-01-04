58e9f3dcf2ea083d2aa7255163cdbdc4d0091936
zhejiang <zhejiang.uk@gmail.com>
2023-01-04 23:18:16 +0000

    Revert the CDC.

--- guardiancouncil/GH_CDC.scala
+++ guardiancouncil/GH_CDC.scala
@@ -109,24 +109,28 @@ class GH_CDCH2LFIFO_HandShake (val params: GH_CDCH2L_Params) extends Module with
     
     // To Low_Freq:
     val cdc_data                                 = WireInit(0.U(params.data_width.W))
-    val can_send                                 = ((io.cdc_slave_busy === 0.U) && (!cdc_channel_empty) && (io.cdc_pull === 0.U))
 
-
-    val fsm_reset :: fsm_sending :: Nil = Enum(2)
+    val fsm_reset :: fsm_sending :: fsm_waiting :: Nil = Enum(3)
     val fsm_state                                = RegInit(fsm_reset)
     switch (fsm_state) {
       is (fsm_reset){
-        cdc_data                                := Mux((can_send === 1.U), cdc_channel_deq_data, 0.U)
-        fsm_state                               := Mux((can_send === 1.U), fsm_sending, fsm_reset)
+        cdc_data                                := Mux(((io.cdc_slave_busy === 0.U) && (!cdc_channel_empty)), cdc_channel_deq_data, 0.U)
+        fsm_state                               := Mux(((io.cdc_slave_busy === 0.U) && (!cdc_channel_empty)), fsm_sending, fsm_reset)
         cdc_channel_deq_ready                   := 0.U
       }
 
       is (fsm_sending){
         cdc_data                                := Mux((io.cdc_pull === 1.U), 0.U, cdc_channel_deq_data)
-        fsm_state                               := Mux((io.cdc_pull === 1.U), fsm_reset, fsm_sending)
+        fsm_state                               := Mux((io.cdc_pull === 1.U), fsm_waiting, fsm_sending)
         cdc_channel_deq_ready                   := Mux((io.cdc_pull === 1.U), 1.U, 0.U)
       }
-  }
+
+      is (fsm_waiting){
+        cdc_data                                := 0.U
+        fsm_state                               := Mux((io.cdc_pull === 1.U), fsm_waiting, fsm_reset)
+        cdc_channel_deq_ready                   := 0.U
+      }
+    }
 
     io.cdc_data_out                               := cdc_data
     io.cdc_busy                                   := cdc_channel.io.status_twoslots
